155e1b498f5f480dce17d76212a54912f86a1773
Monkron <69625827+Monkron@users.noreply.github.com>
2020-08-19 16:41:54 +0700

    Add files via upload

---
+++ Change Bath/Change Bath/Change Bath.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+	int Money;
+	cout << "ENTER YOUR MONEY (THAI BATH)" << endl;
+	cin >> Money;
+	cout <<Money*0.032<<" " << "Dollar \n";
+	cout <<Money*0.027<<" "<< "Euro \n";
+	cout <<Money*0.22<<" " << "Yuan \n";
+	cout <<Money*3.42<< " " << "Yen \n";
+	cout <<Money*0.0000027<< " " << "Bitcoin \n";
+	
+	return 0;
+}
